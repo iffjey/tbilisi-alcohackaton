73826a1545584d204249e23d7101b3f0ae15703b
Valery Bugakov <skymk1@gmail.com>
2018-05-17 00:52:27 +0400

    Added emotion

---
+++ src/components/ExtensionOrLife/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ExtensionOrLife extends Component {
+  render() {
+    return (
+      <div>
+        Extension?
+      </div>
+    );
+  }
+}
+
+export default ExtensionOrLife;
